a4a122dd3e8964e37ed5acca46698053ebe76535
MoizRed <abdoureduniv2000@gmail.com>
2024-11-28 07:38:27 +0100

    some changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import Card from "./Card.tsx";
-import { createContext, useState } from "react";
-export const username = createContext();
+
 type Props = {};
 
 export default function App({}: Props) {
-  const [state, setstate] = useState("");
 
   return (
     <>
-      <username.Provider value={{ state, setstate }}>
-        <Card />
-      </username.Provider>
+  
     </>
   );
 }
